82a6a56e0a4c7d5b5d54d7295d47493d7d28d7d7
majunjie@priceegg.com <1914632129a>
2020-02-20 09:23:57 +0800

    edit

---
+++ src/main/java/cn/demo/dfs/utils/KafkaUtils.java
@@ -0,0 +1,23 @@
+package cn.demo.dfs.utils;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class KafkaUtils {
+    @KafkaListener(topics = {"test"})
+    public void lis(ConsumerRecord<?, ?> record) {
+        Optional<?> kafkaMessage = Optional.ofNullable(record.value());
+
+        if (kafkaMessage.isPresent()) {
+            Object message = kafkaMessage.get();
+            System.out.println("----------------- record =" + record);
+            System.out.println("------------------ message =" + message);
+        }
+
+
+    }
+}
